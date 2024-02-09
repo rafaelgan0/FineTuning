c451a63bf7af8b8a5773fc20a91f7acad97820bb
Rafael Gan <rafaelamielgan@gmail.com>
2024-02-09 01:32:00 -0700

    debugging

--- Test/mistral-7b/PreTrainedModelTest/test.py
+++ Test/mistral-7b/PreTrainedModelTest/test.py
@@ -12,44 +12,49 @@ if torch.cuda.is_available():
 else:
     dev = "cpu"
 device = torch.device(dev)
-
 print("loading model")
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", torch_dtype=torch.float16, trust_remote_code=True)
 model.to(device)
 print("finished loading")
 
-def generate_output(inputStr):
-    inputs = tokenizer(inputStr, return_tensors='pt').to(device)
-    output = model.generate(inputs, max_new_tokens=10, num_return_sequences=1, no_repeat_ngram_size=2, do_sample=True, pad_token_id=tokenizer.eos_token_id)
-    new_output = output[0, inputs.shape[1]:]
-    outputStr = tokenizer(new_output, skip_special_tokens=True)
-    return outputStr
+def generate_output(messages):
+    encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
+
+    model_inputs = encodeds.to(device)
+    model.to(device)
+    generated_ids = model.generate(model_inputs, max_new_tokens=10, do_sample=True)
+    decoded = tokenizer.batch_decode(generated_ids)
+    
+    return decoded[0]
 
 def score_review(sentence):
-    prompt = f"""<|user|>
-    Classify the following review as 'J', 'W' or 'V', where:
-    'J' is positive or encouraging or constructive or specific or has a positive tone or consists of factual statements and a positive tone.
-    'J' is positive.
-    'J' is encouraging.
-    'J' is constructive.
-    'J' is specific.
-    'J' has a positive tone.
-    'J' consists of factual statements and a positive tone.
-    'V' contains personal attacks.
-    'V' contains excessive negativity without constructive feedback.
-    'W' is not encouraging.
-    'W' is not discouraging.
+    prompt = [
+        {"role": "assistant", "content": f"""
+        Classify the following review as 'J', 'W' or 'V', where:
+        'J' is positive or encouraging or constructive or specific or has a positive tone or consists of factual statements and a positive tone.
+        'J' is positive.
+        'J' is encouraging.
+        'J' is constructive.
+        'J' is specific.
+        'J' has a positive tone.
+        'J' consists of factual statements and a positive tone.
+        'V' contains personal attacks.
+        'V' contains excessive negativity without constructive feedback.
+        'W' is not encouraging.
+        'W' is not discouraging.
 
-    Review = "{sentence}"
+        Review = "{sentence}"
 
-    Please classify this review as either 'J', 'W' or 'V'. Only output 'J', 'W' or 'V' with no additional explanation.
-    Your classification:
-    <|end of text|>
-    <|assistant|>
-    """
+        Please classify this review as either 'J', 'W' or 'V'. Only output 'J', 'W' or 'V' with no additional explanation.
+        Your classification:
+        <|end of text|>
+        """}
+    ]
+
 
     score = generate_output(prompt)
+    print(score)
     res = 0
     if 'v' in score.lower():
         res = '2'
@@ -64,11 +69,13 @@ agreementScore = 0
 file_path = '../../../Data/CombinedReviews/comprehensive_combined_annotations.csv'
 
 reviews = pd.read_csv(file_path)
-
+number = 0
 print(reviews)
 
 for index, review in reviews.iterrows():
     if review["majority"] == score_review(review["review"]):
         agreementScore += 1
+        number += 1
+        print(f"""Review {number}""")
 
 print(f"Agreement Score with Mistral 7B: {agreementScore}/99")
