2b7c8b62a5dcecae8fdebf2591561ee834807063
Rafael Gan <rafaelamielgan@gmail.com>
2024-02-12 16:35:58 -0700

    updating script so that we are using multiple gpus to load the model

--- Test/PreTrainedModelTest/test.py
+++ Test/PreTrainedModelTest/test.py
@@ -21,18 +21,21 @@ class ModelLoader:
         if self.load_8bit:
             print(f"Loading {self.model_id} in 8bit...")
             self.tokenizer = AutoTokenizer.from_pretrained(self.model_id, trust_remote_code=True)
-            self.model = AutoModelForCausalLM.from_pretrained(self.model_id, load_in_8bit=True, trust_remote_code=True, device_map = 'auto')
+            self.model = AutoModelForCausalLM.from_pretrained(self.model_id, load_in_8bit=True, trust_remote_code=True)
             print("Finished Loading.")
         elif self.load_16bit:
             print(f"Loading {self.model_id} in 16bit...")
             self.tokenizer = AutoTokenizer.from_pretrained(self.model_id, trust_remote_code=True)
-            self.model = AutoModelForCausalLM.from_pretrained(self.model_id, torch_dtype=torch.float16, trust_remote_code=True, device_map = 'auto')
+            self.model = AutoModelForCausalLM.from_pretrained(self.model_id, torch_dtype=torch.float16, trust_remote_code=True).to("cuda")
             print("Finished Loading.")
         else:
             print(f"Loading {self.model_id}...")
             self.tokenizer = AutoTokenizer.from_pretrained(self.model_id, trust_remote_code=True)
-            self.model = AutoModelForCausalLM.from_pretrained(self.model_id, trust_remote_code=True, device_map = 'auto')
+            self.model = AutoModelForCausalLM.from_pretrained(self.model_id, trust_remote_code=True).to("cuda")
             print("Finished Loading.")
+            
+        if torch.cuda.device_count() > 1:
+            self.model = nn.DataParallel(self.model)
 
     def generate_output(self, max_new_tokens, inputStr):
         if self.model == None:
@@ -68,21 +71,19 @@ def score_review(model_loader, review, config):
       score = '1'
     return score
 
-def evaluate_prompt(config):
-    model_loader = ModelLoader(config.model_name, load_8bit=config.load_8bit, load_16bit=config.load_16bit)
-    model_loader.load_model()
+def evaluate_prompt(config, model_loader, input_file, output_file):
     match_counter = 0
-    
-    df = pd.read_csv('../../Data/CombinedReviews/comprehensive_combined_annotations.csv')
-    #df = df.head(50)
+
+    df = pd.read_csv(input_file)
+    df = df.head(1)
     results = pd.DataFrame(columns=['review_id', 'word_count', 'score', 'score_time', 'majority'])
-    
+
     for index, row in df.iterrows():
         review = row['review']
         majority = row['majority']
         review_id = row['id']
         review = review.replace('"', "'")
-        
+
         if pd.notnull(review) and review != '':
             start_time = time.time()
             score = score_review(model_loader, review, config)
@@ -90,21 +91,25 @@ def evaluate_prompt(config):
             print(score)
             word_count = len(review.split())
             score_time = end_time - start_time
-            
+
             try:
                 if int(score) == int(majority):
                     match_counter += 1
             except Exception:
                 pass
             results.loc[len(results)] = [review_id, word_count, score, score_time, majority]
-            
+
+            review_padded = str(row['id']).ljust(15, ' ')
+            print(review_id, score, majority)
     match_percentage = (match_counter / len(df)) * 100
     print(f"Match Count: {match_counter}")
     print(f"Match Percentage: {match_percentage}%")
-    results.to_csv('evaluate_model.csv', index=False)
-    del model_loader.model
+    results.to_csv(f'{output_file}', index=False)
     torch.cuda.empty_cache()
 ########################################################################
     
-config = Config(prompts.zephyr_p1, 'stabilityai/stablelm-zephyr-3b')
-evaluate_prompt(config)
+config = Config(prompts.mistral_p1, "mistralai/Mistral-7B-Instruct-v0.2", load_16bit=True)
+model_loader = ModelLoader(config.model_name, load_8bit=config.load_8bit, load_16bit=config.load_16bit)
+model_loader.load_model()
+
+evaluate_prompt(config, model_loader, "../../Data/CombinedReviews/comprehensive_combined_annotations.csv", "test_output.csv")
