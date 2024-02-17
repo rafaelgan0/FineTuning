5a0f7ac621d3693485cfcd8aa621136e1bc7e6be
hramali1 <117710499+hramali1@users.noreply.github.com>
2024-02-16 22:38:47 -0700

    Update test.py

--- Test/PreTrainedModelTest/test.py
+++ Test/PreTrainedModelTest/test.py
@@ -100,7 +100,7 @@ def evaluate_prompt(config, model_loader, input_file, output_file):
 num_gpus = torch.cuda.device_count()
 print(f"Number of GPUs: {num_gpus}")
 
-config = Config(prompts.mistral_p1, "mistralai/Mistral-7B-Instruct-v0.2", load_16bit=True)
+config = Config(prompts.mistral_p1, "'stabilityai/stablelm-zephyr-3b", load_8bit=True)
 model_loader = ModelLoader(config.model_name, load_8bit=config.load_8bit, load_16bit=config.load_16bit)
 model_loader.load_model()
 print(model_loader.model.memory_allocated())
